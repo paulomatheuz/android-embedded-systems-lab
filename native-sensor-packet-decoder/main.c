13d8a78119877a2dd299aad33aa81ffbd83a8055
paulomatheuz <paulo.okkotsu@gmail.com>
2026-09-19 09:15:26 -0400

    feat(c): valida byte inicial do pacote

--- native-sensor-packet-decoder/main.c
+++ native-sensor-packet-decoder/main.c
@@ -4,7 +4,14 @@
 int main(void)
 {
     uint8_t pacote[] = {0xAA, 0x01, 0x17, 0x64, 0xD8};
-	printf("%02X %02X\n", (unsigned) pacote[0], (unsigned) pacote[4]);
+
+    printf("%02X %02X\n", (unsigned) pacote[0], (unsigned) pacote[4]);
+
+    if (pacote[0] == 0xAA) {
+        printf("Pacote valido!\n");
+    } else {
+        printf("Pacote invalido!\n");
+    }
 
     return 0;
 }
