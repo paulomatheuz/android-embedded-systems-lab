3641a8f21cd8b1adca0031c6433db3b7a24126dd
paulomatheuz <paulo.okkotsu@gmail.com>
2026-09-24 12:02:30 -0400

    questão 02: cartão de apresentação

---
+++ kotlin-fundamentals/exercicios/01-estrutura-do-programa/02-cartao-apresentacao/Main.kt
@@ -0,0 +1,12 @@
+/*
+ * Questão 2 — Cartão de apresentação
+ *
+ * Exiba três linhas com os rótulos "Nome:", "Curso:" e "Objetivo:".
+ * Preencha cada linha com informações à sua escolha.
+ */
+
+fun main() {
+    println("Nome: Paulo Matheus Cavalcante")
+    println("Curso: Engenharia de Software.")
+    println("Objetivo: Adquirir experiência para entregar bons resultados.")
+}
