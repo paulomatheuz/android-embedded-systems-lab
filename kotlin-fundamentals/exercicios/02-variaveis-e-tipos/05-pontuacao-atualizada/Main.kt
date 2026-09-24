be073f2148b55d5bc2959864ff98fd2c3c60e438
paulomatheuz <paulo.okkotsu@gmail.com>
2026-09-24 12:44:16 -0400

    questão 05: pontuação atualizada

---
+++ kotlin-fundamentals/exercicios/02-variaveis-e-tipos/05-pontuacao-atualizada/Main.kt
@@ -0,0 +1,15 @@
+    /*
+    * Questão 5 — Pontuação atualizada
+    *
+    * Uma pessoa começa com 10 pontos. Exiba a pontuação inicial, altere seu
+    * valor para 25 e exiba a pontuação final. Escolha entre val e var conforme
+    * a necessidade de cada variável.
+    */
+
+    fun main() {
+        var pontuacaoInicial: Int = 10
+        println("Pontuação inicial: $pontuacaoInicial")
+
+        pontuacaoInicial = 25
+        println("Pontuacao final: $pontuacaoInicial")
+    }
