340e22c6a75593a1f898f086bdfb8fb0aa6075d2
paulomatheuz <paulo.okkotsu@gmail.com>
2026-09-24 12:02:22 -0400

    questão 01: primeira execução

---
+++ kotlin-fundamentals/exercicios/01-estrutura-do-programa/01-ola-kotlin/Main.kt
@@ -0,0 +1,11 @@
+/*
+ * Questão 1 — Primeira execução
+ *
+ * Crie um programa que exiba exatamente duas linhas:
+ * "Olá, Kotlin!" e "Meu primeiro programa está funcionando."
+ */
+
+fun main() {
+    println("Olá, Kotlin!")
+    println("Meu primeiro programa está funcionando.")
+}
