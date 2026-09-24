0eeed72d1d87a02261c34be2c4ce8d2a079922e8
paulomatheuz <paulo.okkotsu@gmail.com>
2026-09-24 12:02:41 -0400

    questão 03: triângulo de asteriscos

---
+++ kotlin-fundamentals/exercicios/01-estrutura-do-programa/03-triangulo-asteriscos/Main.kt
@@ -0,0 +1,12 @@
+/*
+ * Questão 3 — Desenho no terminal
+ *
+ * Exiba um triângulo de três linhas usando asteriscos. A primeira linha
+ * deve ter um asterisco, a segunda dois e a terceira três.
+ */
+
+fun main() {
+    println("*")
+    println("**")
+    println("***")
+}
